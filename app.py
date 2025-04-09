da6b10f5d937ec4182d0e00dd48e84508f2e28f2
hahsim2444 <96821915+Pwoerlevel@users.noreply.github.com>
2025-04-10 01:54:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # 👈 هذا يسمح بالوصول من أي Origin
 
 @app.route('/api/test', methods=['POST'])
 def test_api():
