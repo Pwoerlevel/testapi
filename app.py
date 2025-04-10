ca50b1f015ac9f05c2cb2379a5c016046ad9515e
hahsim2444 <96821915+Pwoerlevel@users.noreply.github.com>
2025-04-10 02:10:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from g4f.client import Client
 
 app = Flask(__name__)
 CORS(app)  # السماح بأي origin
 
-client = Client()  # g4f client
 
 @app.route('/api/test', methods=['POST'])
 def test_api():
