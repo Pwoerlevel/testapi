a0f6cf6e52aa1b9242ac943d84a2433846996c44
hahsim2444 <96821915+Pwoerlevel@users.noreply.github.com>
2025-04-10 02:09:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,34 +11,10 @@ client = Client()  # g4f client
 def test_api():
     return jsonify({"message": "Request received successfully ✅"})
 
-@app.route('/api/chat', methods=['POST'])
-def chat_api():
-    data = request.get_json()
-    user_message = data.get("message", "")
+@app.route('/hello/<name>', methods=['POST'])
+def test_api():
+    return jsonify({"message": f"hello {name} "})
 
-    if not user_message:
-        return jsonify({"error": "No message provided"}), 400
-
-    # إرسال الرسالة إلى g4f
-    try:
-        stream = client.chat.completions.create(
-            model="gpt-4",
-            messages=[{"role": "user", "content": user_message}],
-            stream=True,
-            web_search=False
-        )
-
-        # تجميع الرد من الـ stream
-        full_response = ""
-        for chunk in stream:
-            content = chunk.choices[0].delta.content
-            if content:
-                full_response += content
-
-        return jsonify({"response": full_response})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
